8624219b05f50f2ea008f93ff2da9a13a3bb17c4
Brian <riceb53@gmail.com>
2019-02-20 19:32:52 -0600

    add intern and EmailReportable module

---
+++ intern.rb
@@ -0,0 +1,117 @@
+module EmailReportable
+  def send_report
+    p "totally about to go send a report...."
+    # write some code in here to send the report
+    p "definitely sent the report..."
+  end
+end
+
+
+class Employee
+  attr_reader :first_name, :last_name, :salary
+  attr_writer :first_name, :active
+
+  # { :last_name => "Campos", :salary => 70000, :active => false, :first_name => "Danilo"}
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+  end
+
+  def print_info
+    p "#{first_name} #{last_name} makes $#{salary} per year"
+  end
+
+  def give_annual_raise
+    @salary *= 1.05
+  end
+end
+
+employee1 = Employee.new(
+  {
+    :last_name => "Campos",
+    :salary => 70000,
+    :active => false,
+    :first_name => "Danilo"
+  }
+)
+# employee1.print_info
+
+# employee1.give_annual_raise
+
+# employee1.print_info
+
+# employee2 = Employee.new({:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true})
+employee2 = Employee.new(first_name: "Majora", last_name: "Carter", salary: 80000, active: true)
+# employee2.print_info
+
+# employee2.give_annual_raise
+
+# employee2.print_info
+
+
+# p employee1.first_name
+# # employee1.first_name=("Bob")
+# # employee1.first_name = gets.chomp
+# employee1.first_name = "Bob"
+# p "new name"
+# p employee1.first_name
+
+
+class Manager < Employee
+  include EmailReportable
+
+  def initialize(input_options)
+    super
+    @employees = input_options[:employees]
+  end
+
+  def give_all_raises
+    # get all the employees
+    # loop through them
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+    # give each of them a raise
+  end
+
+  def fire_all_employees
+    # get all the employees
+    # loop through them
+    @employees.each do |employee|
+      employee.active = false
+    end
+    # fire them
+  end
+end
+
+manager = Manager.new(first_name: "Manny", last_name: "Bossman", salary: 100000, active: true, employees: [employee1, employee2])
+
+
+
+manager.fire_all_employees
+
+p employee1
+p employee2
+
+# manager.print_info
+manager.send_report
+# p manager
+
+# write a give_all_raises method
+# write a fire_all_employees method
+
+
+
+# an intern is like an employee and they can also send reports
+
+class Intern < Employee
+  include EmailReportable
+end
+
+intern = Intern.new(first_name: "Ingid", last_name: "Isrignhausen", salary: 0, active: true)
+
+intern.print_info
+intern.send_report
+
